644c1d33013e507a1302a49c6c3772dd41ee211c
Amritha raj herle <herleraja@gmail.com>
2024-11-10 10:28:18 -0600

    chore: bump packages

--- packages/react/config/jest/setupTest.js
+++ packages/react/config/jest/setupTest.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import 'jest-styled-components';
 import '@testing-library/jest-dom';
 import { resetIdCounter } from 'downshift';
@@ -11,6 +12,7 @@ async function toBeAccessible(node, label) {
   // we are running a test suite in node instead of jsdom. As a result, we only
   // initialize it if this matcher is called
   if (!aChecker) {
+    // eslint-disable-next-line global-require
     aChecker = require('accessibility-checker');
   }
 
@@ -30,10 +32,6 @@ expect.extend({ toBeAccessible });
 
 beforeEach(() => {
   jest.spyOn(uniqueHook, 'useUniqueId').mockImplementation(() => undefined);
-
-  // Every test we write should have at least one assertion. If this fails, we need to look at the invoking test to ensure there's not a promise being swallowed or something.
-  // Review this for more context: https://github.com/carbon-design-system/carbon-addons-iot-react/issues/1143#issuecomment-623577505
-  expect.hasAssertions();
 
   // Ensure that downshift's internal ID is always 0, otherwise snapshots will change unnecessarily when unrelated snaps are regenerated.
   resetIdCounter();
